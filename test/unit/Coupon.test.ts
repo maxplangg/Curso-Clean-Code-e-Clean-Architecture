ec9550ff2a69534022da742cef24c85f40cb0af5
Max Plangg <pcf.maximilian@gmail.com>
2022-02-28 18:19:20 -0300

    Refactor criando estrutura de pastas respeitando clean architecture; criado classes e testes para atender a useCase PlaceOrder;

--- test/unit/Coupon.test.ts
+++ test/unit/Coupon.test.ts
@@ -1,4 +1,4 @@
-import Coupon from "../src/Coupon";
+import Coupon from "../../src/domain/entity/Coupon";
 
 test("Deve criar um cupom válido", function () {
     const coupon = new Coupon("MAX20", 20);
